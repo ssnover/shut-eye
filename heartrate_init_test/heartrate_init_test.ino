cd13a34bbda93da689fa607ad777db33defc3fdb
Aaron Mallabar <amallabar13@gmail.com>
2018-02-17 11:30:04 -0500

    Decent Heart Rate Monitor, with averaging. Averaging is wrong because of invalid samples

--- heartrate_init_test/heartrate_init_test.ino
+++ heartrate_init_test/heartrate_init_test.ino
@@ -12,7 +12,12 @@ int numBeats;
 int Rate = 0;
 int raw_out[255] = {0};
 int sample_index = 0;
+
+int Bpm = 0;
+int averageBpm = 0;
     
+int getRate();
+int getAverageBpm(int sec);
 
 void setup()
 {
@@ -36,39 +41,70 @@ void callback()
 
 void loop()
 {
-  while (loopFlag == 1) //Run through this loop every (Period) seconds 
-  {
-    raw_out[sample_index] = analogRead(HeartSensor); //Read the sensorer
-    if (raw_out[sample_index] > beatThreshold && raw_out[sample_index-1] < beatThreshold)
-    { 
-      digitalWrite(beatLED, HIGH);
-    }
-    else if (raw_out[sample_index] < beatThreshold && raw_out[sample_index-1] > beatThreshold)
-    {
-      digitalWrite(beatLED, LOW);
-    }
-    
-    if (sample_index > 100)
-    {
-      for(int i = sample_index-100; i < (sample_index-1); i++)
-      {
-       if (raw_out[i] > beatThreshold && raw_out[i-1] < beatThreshold && raw_out[i-2] < beatThreshold && raw_out[i-3] < beatThreshold )   
-       {
-        numBeats++;
-       } 
-      }
-      Rate = numBeats * 6;
-      if (Rate < 200 && Rate > 30)
-      {
-        Serial.print(Rate); Serial.println(" Bpm");
-      }
-      else
-      {
-        Serial.println("Invalid Heart Rate");
-      }
-       numBeats = 0;
-    }
+  Serial.print("MainLoop @: "); Serial.println(t);
+  averageBpm = getAverage(60);
+  Serial.print("---------AVG BPM: "); Serial.print(averageBpm); Serial.println("-----------------");
+}
 
+int getRate()
+{
+  raw_out[sample_index] = analogRead(HeartSensor); //Read the sensorer
+  if (raw_out[sample_index] > beatThreshold && raw_out[sample_index-1] < beatThreshold)
+  { 
+    digitalWrite(beatLED, HIGH);
+  }
+  else if (raw_out[sample_index] < beatThreshold && raw_out[sample_index-1] > beatThreshold)
+  {
+    digitalWrite(beatLED, LOW);
+  } 
+  if (sample_index > 100)
+  {
+    for(int i = sample_index-100; i < (sample_index-1); i++)
+    {
+      if (raw_out[i] > beatThreshold && raw_out[i-1] < beatThreshold && raw_out[i-2] < beatThreshold && raw_out[i-3] < beatThreshold )   
+      {
+        numBeats++;
+      } 
+    }
+    Rate = numBeats * 6;
+    numBeats = 0;
+  }
+  return Rate;
+}
+
+int getAverage(int sec)
+{
+  Serial.print("Enter_getaverage @: "); Serial.println(t);
+  int avg = 0;
+  int numSamples = 0;
+  int BpmSum = 0;
+  int Badrdg = 0;
+  t = 0;
+
+  while (t <= sec)
+  {
+    while (loopFlag == 0)
+    {
+      //Serial.print("StartSpinning @: "); Serial.println(t);
+      Serial.print("");
+      //spin
+    }
+    //Serial.print("LeaveSpinning @: "); Serial.println(t);
+    Bpm = getRate();
+    if (Bpm < 200 && Bpm > 30)
+    {
+     Serial.print(Bpm); Serial.println(" Bpm");
+     BpmSum = BpmSum + Bpm;
+    }
+    else
+    {
+     Serial.print("Invalid Heart Rate: #"); Serial.print(Badrdg+1); Serial.print(" : "); Serial.println(Bpm);
+     Badrdg ++;
+    }
     loopFlag = 0;
   }
+  numSamples = (sec / Period) - Badrdg;
+  avg = BpmSum / numSamples;
+  return avg;
 }
+
